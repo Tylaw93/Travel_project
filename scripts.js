58cc82f58a4a4772c1c978724faa6f5c9c944c06
Tyler Lawson <tylaw93@yahoo.com>
2021-06-12 13:02:06 -0500

    Added fetch
    fetching from opentripmap

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
+const apikey = "5ae2e3f221c38a28845f05b63b141ff8b556302ea945abb40cb4ffe1";
+const cityName = "St. Louis";
+const getLatLon = `https://api.opentripmap.com/0.1/en/places/geoname?name=${cityName}&apikey=${apikey}`;
 
+fetch(getLatLon)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
+
+fetch().then(function (params) {});
