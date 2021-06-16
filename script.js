2d4fd802f40d3995d828bdfb7418baf53ea1ba7e
Tyler Lawson <tylaw93@yahoo.com>
2021-06-15 20:50:45 -0500

    Fixing jquery issue

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const apikey = "5ae2e3f221c38a28845f05b63b141ff8b556302ea945abb40cb4ffe1";
 // eslint-disable-next-line no-undef
-const cityName = $("#city").val();
+const cityName = "St. Louis";
 const getLatLon = `https://api.opentripmap.com/0.1/en/places/geoname?name=${cityName}&apikey=${apikey}`;
 
 fetch(getLatLon)
