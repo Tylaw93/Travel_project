0a8e2683a4c375dd32ddf291a55d67d3572eefc4
Tyler Lawson <tylaw93@yahoo.com>
2021-06-17 16:48:04 -0500

    Added Event Listener
    Using the search bar submit takes the city name and uses it in the api

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
 const apikey = "5ae2e3f221c38a28845f05b63b141ff8b556302ea945abb40cb4ffe1";
 // eslint-disable-next-line no-undef
-const cityName = "St. Louis";
-const getLatLon = `https://api.opentripmap.com/0.1/en/places/geoname?name=${cityName}&apikey=${apikey}`;
+// const cityName = "St. Louis";
 
-// eslint-disable-next-line no-undef
-
-fetch(getLatLon)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-    // eslint-disable-next-line no-undef
-    mapboxgl.accessToken =
-      "pk.eyJ1IjoidHlsYXc5MyIsImEiOiJja3B1N3hyeG8wdmRxMnVvNnNxa2VuaG5iIn0.lMBPEZ7KGl2Y2nJk0u1RDQ";
-    // eslint-disable-next-line no-undef
-    // eslint-disable-next-line no-unused-vars
-    // eslint-disable-next-line prefer-const
-    // eslint-disable-next-line no-undef
-    const map = new mapboxgl.Map({
-      container: "map", // container ID
-      style: "mapbox://styles/mapbox/streets-v11", // style URL
-      center: [data.lon, data.lat], // starting position [lng, lat]
-      zoom: 9, // starting zoom
+document.addEventListener("submit", getCityName);
+function getCityName(params) {
+  const cityName = document.querySelector("#city").value;
+  // eslint-disable-next-line no-undef
+  const getLatLon = `https://api.opentripmap.com/0.1/en/places/geoname?name=${cityName}&apikey=${apikey}`;
+  fetch(getLatLon)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      // eslint-disable-next-line no-undef
+      mapboxgl.accessToken =
+        "pk.eyJ1IjoidHlsYXc5MyIsImEiOiJja3B1N3hyeG8wdmRxMnVvNnNxa2VuaG5iIn0.lMBPEZ7KGl2Y2nJk0u1RDQ";
+      // eslint-disable-next-line no-undef
+      // eslint-disable-next-line no-unused-vars
+      // eslint-disable-next-line prefer-const
+      // eslint-disable-next-line no-undef
+      const map = new mapboxgl.Map({
+        container: "map", // container ID
+        style: "mapbox://styles/mapbox/streets-v11", // style URL
+        center: [data.lon, data.lat], // starting position [lng, lat]
+        zoom: 9, // starting zoom
+      });
     });
-  });
+}
