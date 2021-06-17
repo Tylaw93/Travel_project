184b3cacb86231018f558af54ad55c165aecb1df
Tyler Lawson <tylaw93@yahoo.com>
2021-06-17 16:45:52 -0500

    Added lat lon search
    Using the a fetch request to get a cities lat and lon and pluging that into the map

--- script.js
+++ script.js
@@ -4,18 +4,6 @@ const cityName = "St. Louis";
 const getLatLon = `https://api.opentripmap.com/0.1/en/places/geoname?name=${cityName}&apikey=${apikey}`;
 
 // eslint-disable-next-line no-undef
-mapboxgl.accessToken =
-  "pk.eyJ1IjoidHlsYXc5MyIsImEiOiJja3B1N3hyeG8wdmRxMnVvNnNxa2VuaG5iIn0.lMBPEZ7KGl2Y2nJk0u1RDQ";
-// eslint-disable-next-line no-undef
-// eslint-disable-next-line no-unused-vars
-// eslint-disable-next-line prefer-const
-// eslint-disable-next-line no-undef
-const map = new mapboxgl.Map({
-  container: "map", // container ID
-  style: "mapbox://styles/mapbox/streets-v11", // style URL
-  center: [-74.5, 40], // starting position [lng, lat]
-  zoom: 9, // starting zoom
-});
 
 fetch(getLatLon)
   .then(function (response) {
@@ -23,4 +11,17 @@ fetch(getLatLon)
   })
   .then(function (data) {
     console.log(data);
+    // eslint-disable-next-line no-undef
+    mapboxgl.accessToken =
+      "pk.eyJ1IjoidHlsYXc5MyIsImEiOiJja3B1N3hyeG8wdmRxMnVvNnNxa2VuaG5iIn0.lMBPEZ7KGl2Y2nJk0u1RDQ";
+    // eslint-disable-next-line no-undef
+    // eslint-disable-next-line no-unused-vars
+    // eslint-disable-next-line prefer-const
+    // eslint-disable-next-line no-undef
+    const map = new mapboxgl.Map({
+      container: "map", // container ID
+      style: "mapbox://styles/mapbox/streets-v11", // style URL
+      center: [data.lon, data.lat], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
   });
