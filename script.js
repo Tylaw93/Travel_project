c5dfea7c7fcd5927efc30f85d1fc26124be8992f
Tyler Lawson <tylaw93@yahoo.com>
2021-06-17 19:43:08 -0500

    Adding pop ups
    Pulled some sample code from api docs and refactoring it to fit the project

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 /* eslint-disable prefer-const */
 const apikey = "5ae2e3f221c38a28845f05b63b141ff8b556302ea945abb40cb4ffe1";
@@ -5,6 +6,17 @@ const apikey = "5ae2e3f221c38a28845f05b63b141ff8b556302ea945abb40cb4ffe1";
 // const cityName = "St. Louis";
 
 document.addEventListener("submit", getCityName);
+
+function getLocalAttractions(data) {
+  const atttractions = `https://api.opentripmap.com/0.1/en/places/radius?radius=200&lon=${data.lon}&${data.lat}&apikey=${apikey}`;
+  fetch(atttractions)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (museums) {
+      console.log(museums);
+    });
+}
 function getCityName(params) {
   const cityName = document.querySelector("#city").value;
   // eslint-disable-next-line no-undef
@@ -28,9 +40,54 @@ function getCityName(params) {
         center: [data.lon, data.lat], // starting position [lng, lat]
         zoom: 10, // starting zoom
       });
-      let popup = new mapboxgl.Popup({
-        closeButton: false,
-        closeOnClick: false,
+      fetch();
+      map.on("load", function () {
+        map.addSource("places", {
+          type: "geojson",
+          data: { data },
+        });
+        // Add a layer showing the places.
+        map.addLayer({
+          id: "places",
+          type: "circle",
+          source: "places",
+          paint: {
+            "circle-color": "#4264fb",
+            "circle-radius": 6,
+            "circle-stroke-width": 2,
+            "circle-stroke-color": "#ffffff",
+          },
+        });
+
+        // Create a popup, but don't add it to the map yet.
+        let popup = new mapboxgl.Popup({
+          closeButton: false,
+          closeOnClick: false,
+        });
+
+        map.on("mouseenter", "places", function (e) {
+          // Change the cursor style as a UI indicator.
+          map.getCanvas().style.cursor = "pointer";
+
+          let coordinates = e.features[0].geometry.coordinates.slice();
+          let description = e.features[0].properties.description;
+
+          // Ensure that if the map is zoomed out such that multiple
+          // copies of the feature are visible, the popup appears
+          // over the copy being pointed to.
+          while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+          }
+
+          // Populate the popup and set its coordinates
+          // based on the feature found.
+          popup.setLngLat(coordinates).setHTML(description).addTo(map);
+        });
+
+        map.on("mouseleave", "places", function () {
+          map.getCanvas().style.cursor = "";
+          popup.remove();
+        });
       });
     });
 }
