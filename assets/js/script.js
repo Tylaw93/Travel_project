3c64ebd3b5a4f96b0538ecca5309c87d2d790e59
Tyler Lawson <tylaw93@yahoo.com>
2021-06-22 21:09:18 -0500

    Added city cards with location names for the favorited items in the city

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,22 +13,31 @@ we need to set #locationImg and #locationDesc .innerHTML("") when beginning sear
 
 console.log(localStorage);
 renderFavs();
-
 let cities = [];
 let favorites = [];
 let favors = [new Set(favorites)];
 document.addEventListener("submit", getCityName);
 function saveFavs() {
+  localStorage.setItem("Cities", JSON.stringify(cities));
   localStorage.setItem(tempCityName, JSON.stringify(favorites));
 }
 
 function renderFavs() {
   for (let i = 0; i < localStorage.length - 2; i++) {
     console.log(i);
-    let city = localStorage[i];
-    for (let favs = 0; j < city.length; favs++) {
-      let location = city[favs];
-      $("#cityCards").append("");
+    let city = window.localStorage.key(i);
+    console.log(city);
+    let cityNames = JSON.parse(localStorage.getItem(city));
+    console.log(cityNames);
+    for (let favs = 0; favs < cityNames.length; favs++) {
+      let location = cityNames[favs];
+      let newCard =
+        '<div class="favoriteCard flex flex-col justify-center items-center mt-4 p-4 w-full lg:w-2/5 gap-2 lg:gap-4 bg-gray-400 rounded"><p class="flex self-start">' +
+        city +
+        '</p><div class="flex flex-col justify-center items-start gap-2 p-4 bg-gray-200 rounded w-11/12"><p>' +
+        location +
+        '<span><ion-icon class="fav md hydrated text-red-500 hidden" name="heart" role="img" aria-label="heart"></ion-icon>`</span></p></div><button class="flex self-end">REMOVE</button></div>';
+      $("#cityCards").append(newCard);
     }
   }
 }
